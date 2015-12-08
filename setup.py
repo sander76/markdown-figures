104ba1ca1913333ccc0b226cf0f4ff04ae0e060d
sander <s.teunissen@gmail.com>
2015-12-08 20:11:08 +0100

    I guess this should work....

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='markdown-figures',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='sander',
+    author_email='',
+    description=''
+)
